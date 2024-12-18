a6d2bb4974c40ab0cb5d1bac63f6ab5944fbbdce
jakubmalewicz <kubamalewicz002@gmail.com>
2024-12-18 19:52:38 +0100

    Project initialization

---
+++ README.md
@@ -0,0 +1,2 @@
+# Golf App
+This project is a mobile version of already existing [golf app](https://github.com/gmalewicz/golf-app).
---
+++ android/app/src/main/kotlin/malewicz/jakub/golf_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package malewicz.jakub.golf_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
